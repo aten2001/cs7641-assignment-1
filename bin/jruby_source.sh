0976fab0e19f3ee85ffea5df828e799c6c2ac52d
Matt Kirk <matt@matthewkirk.com>
2014-02-14 19:09:18 -0800

    New Build less weirdness with source

--- bin/jruby_source.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd vendor/jruby-1.7.10/bin
-PATH=`pwd`:$PATH
-cd -
-export PATH
--- bin/setup_jruby.sh
+++ bin/setup_jruby.sh
@@ -5,4 +5,8 @@ tar -xzvf jruby-bin-1.7.10.tar.gz
 cd jruby-1.7.10/bin
 export PATH=$PATH:`pwd`
 cd $original_pwd
-source bin/jruby_source.sh
+
+cd vendor/jruby-1.7.10/bin
+PATH=`pwd`:$PATH
+cd -
+export PATH
